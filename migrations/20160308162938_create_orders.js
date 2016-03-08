39cfa6f4e7f4f42de0b4a1d0d53e2dde77478ba1
Daniel Klein <danhirschklein@gmail.com>
2016-03-08 16:53:21 -0700

    Created all table migrations

--- migrations/20160308162938_create_orders.js
+++ migrations/20160308162938_create_orders.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('orders', function(table){
+    table.increments();
+    table.integer('user_id').references('id').inTable('users');
+
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('orders');
 };
+
