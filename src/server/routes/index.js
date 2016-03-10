02cd8537335faee3447f1454aef06ec8e5111a18
Daniel Klein <danhirschklein@gmail.com>
2016-03-10 10:54:17 -0700

    Added charge.html and back end post route for Stripe

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -29,6 +29,24 @@ router.post('/checkout',function (req, res, next) {
   res.send('form': body)
 
 
+router.post('/charge', function(req, res,next) {
+    var stripeToken = req.body.stripeToken;
+    var amount =  req.body.stripeAmount;
+
+    stripe.charges.create({
+        card: stripeToken,
+        currency: 'usd',
+        amount: amount
+    },
+    function(err, charge) {
+        if (err) {
+            res.send('error');
+        } else {
+            res.send('success');
+        }
+    });
+});
+
 }
 
 
